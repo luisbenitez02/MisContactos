a21bd7672e23d4dd792e34cdfe0bc1ddaab8da9e
Luis Benitez <luisbenitez02@gmail.com>
2017-07-12 23:01:17 -0500

    Creamos nuestra toolbar
    creamos nuestra propia toolbar

--- app/src/main/java/com/example/luisb/miscontactos/DetalleContactoActivity.java
+++ app/src/main/java/com/example/luisb/miscontactos/DetalleContactoActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -23,6 +24,9 @@ public class DetalleContactoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_contacto);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);//para que se vea bien en todas las pantallas
 
         //vamos a recibir los parametros
         Bundle parametros = getIntent().getExtras();
